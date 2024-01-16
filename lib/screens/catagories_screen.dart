f954a3d4ea4e592906fb21d97675066a5a96e6ba
Mishad <mishadcs2002@gmail.com>
2024-01-16 23:34:54 +0600

    meal model added

--- lib/screens/catagories_screen.dart
+++ lib/screens/catagories_screen.dart
@@ -5,7 +5,19 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('HELLO'),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: const [
+          Text('1', style: TextStyle(color: Colors.white, fontSize: 30)),
+          Text('1', style: TextStyle(color: Colors.white, fontSize: 30)),
+          //Text(2)
+        ],
+      ),
     );
   }
 }
