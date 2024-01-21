25a797d83d475ca5dd188a53b7a72cedaf8d6406
Mishad <mishadcs2002@gmail.com>
2024-01-21 23:39:17 +0600

    Curve animation

--- lib/screens/catagories_screen.dart
+++ lib/screens/catagories_screen.dart
@@ -77,9 +77,10 @@ class _CategoriesScreenState extends State<CategoriesScreen>
       //   child: child,
       // ),
       builder: (context, child) => SlideTransition(
-        position: _animationController.drive(
-          Tween(begin: const Offset(0, 0.3), end: Offset(0, 0)),
-        ),
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: Offset(0, 0),
+        ).animate(CurvedAnimation(parent: parent, curve: curve)),
         child: child,
       ),
     );
